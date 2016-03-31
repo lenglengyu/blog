6bafb340e35a2d7c92af527e49eb3bc2baa6dd14
王喆 <nasa127@gmail.com>
2016-04-01 01:38:15 +0800

    step 12

--- app.js
+++ app.js
@@ -4,23 +4,10 @@ import ReactDOM from 'react-dom';
 
 
 const MessageBox = React.createClass({
-  getInitialState: function() {
-    return {
-      message: '你好 世界！(state)',
-      clickCount: 0
-    };
-  },
-  clickHandler(){
-    this.setState({
-      clickCount: this.state.clickCount+1
-    });
-  },
   render () {
     return (
       <div>
-        <h1> {this.state.message}</h1>
-        <a onClick={this.clickHandler}>点我</a>
-        <div>你一共点击了我{ this.state.clickCount}下</div>
+        <SubMessage />
       </div>
     )
   }
@@ -28,25 +15,17 @@ const MessageBox = React.createClass({
 
 
 const SubMessage = React.createClass({
+  getDefaultProps: function() {
+    return {
+      message:"人间四月芳菲尽"
+    };
+  },
   render () {
     return (
-      <div>
-        <div>写代码也是艺术</div>
-        <Footer />
-      </div>
+        <div>{ this.props.message }</div>
     )
   }
 })
-
-
-const Footer = React.createClass({
-  render () {
-    return (
-      <div>我们用代码在创作</div>
-    )
-  }
-})
-
 
 ReactDOM.render(
   <MessageBox />,
